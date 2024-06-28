31dd8d0f52a13b818b6e033e00b87531c89ba808
kanggun99 <kanggun99@gmail.com>
2024-06-28 17:42:21 +0900

    Update ATMMain.java

--- KG/src/ATM/ATMMain.java
+++ KG/src/ATM/ATMMain.java
@@ -2,7 +2,6 @@ package ATM;
 
 import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
